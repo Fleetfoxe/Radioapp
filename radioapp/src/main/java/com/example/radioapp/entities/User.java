765fb91c01867bf1e34e85e67e4e2c87d7c5c645
Andreas <Andreas_93l@hotmail.com>
2021-03-17 11:37:25 +0100

    Lagt till "create user" + log in fungerar

--- radioapp/src/main/java/com/example/radioapp/entities/User.java
+++ radioapp/src/main/java/com/example/radioapp/entities/User.java
@@ -1,4 +1,70 @@
 package com.example.radioapp.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String email;
+    String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User() {
+    }
+    /*funkar inte RIKTIGT än
+    public User(String email, String name, String password) {
+        this.email = email;
+        this.name   =name;
+        this.password = password;
+    }
+*/
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @JsonIgnore
+    public String getPassword() {
+        return password;
+    }
+    @JsonProperty
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
--- radioapp/src/main/java/com/example/radioapp/services/UserService.java
+++ radioapp/src/main/java/com/example/radioapp/services/UserService.java
@@ -1,4 +1,30 @@
 package com.example.radioapp.services;
 
+import com.example.radioapp.configs.MyUserDetailService;
+import com.example.radioapp.entities.User;
+import com.example.radioapp.repositories.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+    @Autowired
+    private MyUserDetailService detailService;
+    @Autowired
+    private UserRepo userRepo;
+
+
+    public User register(User user){
+
+        return detailService.registerUser(user);
+    }
+/*  funkar inte RIKTIGT än
+    public List<User> getUsers() {
+        System.out.println("funkar de?");
+        return userRepo.findAll();
+    }
+
+ */
 }
