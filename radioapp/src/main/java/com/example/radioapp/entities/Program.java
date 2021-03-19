9425639075a1569a564b4e4c4f9db0386441ef4f
FCO <franne_@hotmail.com>
2021-03-19 14:20:39 +0100

    lagt till från sr/api

--- radioapp/src/main/java/com/example/radioapp/entities/Program.java
+++ radioapp/src/main/java/com/example/radioapp/entities/Program.java
@@ -3,21 +3,41 @@ package com.example.radioapp.entities;
 import javax.persistence.*;
 
 @Entity
-    @Table(name = "programs")
-    public class Program {
+@Table(name = "programs")
+public class Program {
 
     @Id
+    private String description;
+    private String programCategory;
     private String id;
-    String name;
-    String description;
+    private String name;
 
 
     public Program() {
     }
 
-    public Program(String id, String name) {
+
+    public Program(String description, String programCategory, String id, String name) {
+        this.description = description;
+        this.programCategory = programCategory;
         this.id = id;
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProgramCategory() {
+        return programCategory;
+    }
+
+    public void setProgramCategory(String programCategory) {
+        this.programCategory = programCategory;
     }
 
     public String getId() {
