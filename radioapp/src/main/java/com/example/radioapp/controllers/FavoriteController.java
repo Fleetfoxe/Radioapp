6361e6ab75fa16d1a1f29de2a8e4d368b92a50a0
Peter <p.akesson@gmail.com>
2021-03-22 13:24:09 +0100

    deleted some junk

--- radioapp/src/main/java/com/example/radioapp/controllers/FavoriteController.java
+++ radioapp/src/main/java/com/example/radioapp/controllers/FavoriteController.java
@@ -1,7 +1,6 @@
 package com.example.radioapp.controllers;
 
 import com.example.radioapp.entities.Favorite;
-import com.example.radioapp.services.EpisodeService;
 import com.example.radioapp.services.FavoriteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -12,23 +11,4 @@ public class FavoriteController {
     @Autowired
     private FavoriteService favoriteService;
 
-    @GetMapping("/rest/fav")
-    public String helloworld() {
-        return "Hello World!";
-    }
-
-
-/*
-    @GetMapping("/rest/favorites2")
-    public void createFavorite() {
-        favoriteService.createFavorite();
-    }
-*/
-
-    /*
-    @GetMapping("/rest/favorites/{episodeid}")
-    public void createFavorite(@PathVariable long episodeid) {
-        favoriteService.createFavorite(episodeid);
-    }
-*/
 }
