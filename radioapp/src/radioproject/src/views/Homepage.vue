986d39a80117b2d1085ec5ddbbec930455a77825
Peter <p.akesson@gmail.com>
2021-04-06 14:45:20 +0200

    adding frontend

---
+++ radioapp/src/radioproject/src/views/Homepage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="wrapper">
+<h1>Home</h1>
+  </div>
+</template>
+
+<script>
+
+import ChannelList from "./ChannelList.vue"
+
+export default {
+    name: "homepage",
+    components: {
+      ChannelList
+    }
+}
+</script>
+
+<style scoped>
+
+#wrapper {
+  background-color: lightgray;
+  padding-top: 25px;
+  padding-bottom: 100px;
+  color: black;
+  border-radius: 50px;
+  margin-bottom: 20px;
+}
+
+h1 {
+  font-family: 'Arial';
+  text-align: center;
+  color: black;
+  margin-bottom: 25px;
+}
+
+</style>
---
+++ radioapp/src/radioproject/src/views/ProgramsInChannel.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+<h1>program</h1>
+  </div> 
+
+</template>
+
+<script>
+
+export default {
+
+};
+</script>
+
+<style scoped>
+
+</style>
