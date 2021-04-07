7b2c02445e639ddc6dba347f67bd9c03a58489b5
Peter <p.akesson@gmail.com>
2021-04-07 20:36:25 +0200

    add axios and store.js logic (doesnt work)

--- radioapp/src/frontend/src/store/store.js
+++ radioapp/src/frontend/src/store/store.js
@@ -1,12 +1,34 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    hello: String
   },
   mutations: {
+    setHello(state, payload) {
+      state.hello = payload;
+    }
   },
   actions: {
+    async fetchHello() {
+      await axios.get("http://localhost:8080/api/test/hello")
+      .then(response => {
+        this.commit("setHello", response.data)
+      })
+    }
   },
+
+
+  getters: {
+    getHello(state) {
+      return state.hello
+    }
+  },
+
+
+
+
   modules: {
   }
 })
--- radioapp/src/frontend/src/views/Home.vue
+++ radioapp/src/frontend/src/views/Home.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="home">
     <h1>Welcome!</h1>
+    <h1> {{ getHello }} </h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 
 export default {
   name: 'Home',
-  components: {
+  
+  computed: {
+    getHello() {
+      return this.$store.getters.getHello
+    }
+  },
+
+  mounted() {
+    this.$store.dispatch("fetchHello");
   }
 }
 </script>
