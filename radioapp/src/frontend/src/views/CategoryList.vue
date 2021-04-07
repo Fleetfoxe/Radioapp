e1d282beccd8483b26b8a5e710e56276388bef0d
FCO <franne_@hotmail.com>
2021-04-07 22:25:08 +0200

    Added CategoryList

---
+++ radioapp/src/frontend/src/views/CategoryList.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1>Categories</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
+
+
+<!-- lär användas soon
+<script>
+import Category from "../components/Category.vue"
+export default {
+    name: "categoryListComponent",
+    components: {
+        Category,
+    },
+
+    computed: {
+        categories(){
+            return this.$store.state.categories;
+        },
+    },
+    
+};
+</script>
+--->
+
+<!--
+<div class="category-list">
+        <div v-if="categories.length > 0">
+            <Category
+
+                v-for="(category, i) in categories"
+                :key="i"
+                :category="category"
+            />
+        </div>
+    </div> --->
