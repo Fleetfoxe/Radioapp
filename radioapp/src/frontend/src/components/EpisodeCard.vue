c56dde19e3b11e1f0970b300b18fbbe5d645dc37
Peter <p.akesson@gmail.com>
2021-04-14 10:33:22 +0200

    button to add episode favorite works

--- radioapp/src/frontend/src/components/EpisodeCard.vue
+++ radioapp/src/frontend/src/components/EpisodeCard.vue
@@ -1,15 +1,12 @@
 <template>
-
-
-
-
     <div class="episode-card">
-
-        
         <h2>{{episode.title}}</h2>
         <h2>{{episode.startTimeUtc}}</h2>
 
-        
+<!-- Only show buttons if user is logged in -->
+  <div v-if="isLoggedIn">
+  <button @click="addFavorite(this.episode.episodeid)">Add favorite</button>
+  </div>
 
     </div>
 
@@ -20,16 +17,39 @@
 <script>
 
 export default {
-
     props: ["episode"],
+    computed: {
+    loggedInUser() {
+      return this.$store.state.loggedInUser
+    },
+    isLoggedIn() {
+      return this.loggedInUser != null
+    }
+},
+methods: {
 
+    
+    async addFavorite(episodeId) {
+        //Putting programID in credentials 
+            let credentials = {
+                episodeId: episodeId,
+            }
+            //console.log('credentials are: '+ credentials)
 
-
-
-
-
-
-
+            //Sends credentials as json object to backend
+        let response = await fetch("/rest/favorites/add-favorite", {
+            method: "POST",
+            headers: {'Content-Type': 'application/json' },
+            body: JSON.stringify(credentials)
+      });
+          //Debug
+        if(response.url.includes('error')) {
+            console.log('Error');
+        } else {
+            console.log(response)
+        }
+      },
+}
 }
 
 </script>
--- radioapp/src/frontend/src/components/FavoriteEpCard.vue
+++ radioapp/src/frontend/src/components/FavoriteEpCard.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="favorite-episode-card">
       <h4>{{ favoriteEpisode.title }}</h4>
+      <h4>{{ ourDate }}</h4>
   </div>
 </template>
 
 <script>
 export default {
     props: ["favoriteEpisode", "favoriteEpisodeId" ],
+
+    computed: {
+      ourDate() {
+        return new Date(this.favoriteEpisode.startTimeUtc).toLocaleString();
+
+      }
+    }
 }
 </script>
 
